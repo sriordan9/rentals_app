8076049c9a5404bcb27eeac35426d2ccc996abee
Sean Riordan <riordan.sean9@gmail.com>
2018-03-14 14:38:27 -0400

    Created AvailableForm component

--- src/App.js
+++ src/App.js
@@ -5,14 +5,47 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HtmlMain from './components/htmlMain/HtmlMain';
+import AvailableForm from './components/htmlMain/AvailableForm/AvailableForm';
 
 class App extends Component {
+  state = {
+    headerContent: [
+      {id: '1', linkName: 'Amenities/Features'},
+      {id: '2', linkName: 'Pricing'},
+      {id: '3', linkName: 'Residents'},
+      {id: '4', linkName: 'Contact Us'},
+      {id: '5', phone: '1-800-867-5309'},
+      {id: '6', email: 'jennyjenny@famousong.com'}
+    ],
+    inputs: [
+      [
+        {id: '7', type: 'radio', value:'1'},
+        {id: '8', type: 'radio', value:'2'},
+        {id: '9', type: 'radio', value:'3'}
+      ],
+      [
+        {id: '9', type: 'radio', value:'Yes'},
+        {id: '10', type: 'radio', value:'No'},
+      ],
+      [
+        {id: '11', type: 'radio', value:'Yes'},
+        {id: '12', type: 'radio', value:'No'},
+      ],
+      [
+        {id: '9', type: 'radio', value:'Yes'},
+      ]
+    ]
+  }
+  //create dynamic header
+
+
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <Header/>
-          <HtmlMain/>
+          <Header />
+          <HtmlMain />
+          <AvailableForm inputs={this.state.inputs}/>
           <ul className="App_ul">
             <li>Available Apartments</li>
             <li>Apply Here!</li>
---
+++ src/components/htmlMain/AcctForm/AcctForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const acctForm = () => {
+    return(
+        <div>
+            <form>
+                <input placeholder="FIRST NAME"></input>
+                <input placeholder="LAST NAME"></input>
+                <input placeholder="EMAIL"></input>
+                <input placeholder="PASSWORD"></input>
+            </form>
+        </div>
+    );
+};
+
+export default acctForm;
